b1933ded7ee8c4ab38692ce4914fbb9bc439ccf4
jonathon lee <jonnyleealas@gmail.com>
2020-05-04 14:57:01 -0700

    object literal

---
+++ js/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+
+
+
+var seattle = {
+  minCus : 23,
+  maxCus : 65,
+  aveCookie: 6.3
+};
+
+var tokyo = {
+  minCus: 3,
+  maxCus: 24,
+  aveCookie: 1.2
+};
+
+var dubai = {
+  minCus: 11,
+  maxCus: 38,
+  aveCookie: 3.7
+};
+
+var paris = {
+  minCus: 20,
+  maxCus: 38,
+  aveCookie: 2.3
+};
+
+var lima = {
+  minCus: 2,
+  maxCus: 16,
+  aveCookie: 4.6
+};
+
+var avArray = [6.3, 1.2, 3.7, 2.3, 4.6];
+
+var multiply = (a,b){
+    var ab = a*b;
+}

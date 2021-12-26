a1bd1bfaf54a2bac1e4907e93a37ef03f5599f6f
Jonathon Lee <jonnyleealas@gmail.com>
2021-12-26 16:17:37 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ function StoreLocation(city, minCus, maxCus, aveCookie) {
   this.cookiesSold = [];
   this.aveCookie = aveCookie;
   this.hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total'];
-  //this means everything inside of contructor
+  //this means everything inside of constructor
   arrayCities.push(this);
 }
 
